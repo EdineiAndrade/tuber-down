748f49cbc2ecada7c5ce611e7b2de771f9b7c546
EdineiAndrade <edinei22.ba@gmail.com>
2022-03-02 01:43:06 -0300

    Segundo Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import {useState} from 'react'
+
+const urlDown = "http://54.145.59.200:3000/?url=https://www.youtube.com/watch?v=eDeDcYn3vrs"
+
+
 function App() {
+  const [Download, setDownload] = useState('');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <a
-          className="App-link"
-          href="http://54.145.59.200:3000/?url=https://www.youtube.com/watch?v=eDeDcYn3vrs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <h1>Baixar Videos</h1>
+        <p>
+          
+          <button onClick={()=> setDownload(urlDown)}> Baixar</button>
+          <div className="group">
+            
+            </div>
+        </p>
+        <p>{Download}</p>
+        {Download && <iframe src={Download}></iframe>}
     </div>
-  );
-}
 
+  );
+  }
 export default App;
